f7a9ade792134559c4ab8c0f2a8f57a4f1d26c27
Isaque-Claudino-Escola <86322136+Isaque-Claudino-Escola@users.noreply.github.com>
2024-03-05 21:34:11 -0300

    Update Professor.java

--- PessoaMatricula/Professor.java
+++ PessoaMatricula/Professor.java
@@ -2,4 +2,8 @@ import java.lang.Long;
 
 public class Professor extends Pessoa {
   private Long matriculaFuncional;
+
+  public Long matriculaFuncional() {
+    return this.matriculaFuncional;
+  }
 }
