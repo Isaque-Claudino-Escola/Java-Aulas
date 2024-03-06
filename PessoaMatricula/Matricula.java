e6b0972c56570c8d160556e14490c0a7b788fc64
Isaque-Claudino-Escola <86322136+Isaque-Claudino-Escola@users.noreply.github.com>
2024-03-05 21:17:02 -0300

    Create Matricula.java

---
+++ PessoaMatricula/Matricula.java
@@ -0,0 +1,11 @@
+import java.lang.*
+
+public class Matricula {
+  private Long code;
+  private Curso curso;
+  private Aluno aluno;
+
+  public boolean matricular(Aluno aluno, Curso curso) {
+    System.out.printl("Matricula feita {true}");
+  }
+}
