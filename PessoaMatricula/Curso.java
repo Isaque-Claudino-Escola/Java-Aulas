a5f7d9b69e0527d77736cffb2c43ab7664bd5dbf
Isaque-Claudino-Escola <86322136+Isaque-Claudino-Escola@users.noreply.github.com>
2024-03-05 21:33:23 -0300

    Create Curso.java

---
+++ PessoaMatricula/Curso.java
@@ -0,0 +1,14 @@
+import java.lang.Integer;
+
+public class Curso extends Matricula {
+  private Integer code;
+  private String nome;
+
+  public Integer getCode() {
+    return this.code;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+}
